f253cdebdae348b636b6423aa482c80a0770dcf1
Ziyu Fu <fuziyu0730@gmail.com>
2022-01-06 10:24:22 +0900

    020301

---
+++ s202018021_02-03-ex1/02-03-ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(int argc, char *argv[]){
+
+    printf("There are %d arguments.\n", argc);
+    for (int i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    
+    return 0;
+}
